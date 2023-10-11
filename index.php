a8279fadafd4f25d0e0dd4d3fdba6f6775d5b20d
THEFLUTE <137923823+THEFLUTE@users.noreply.github.com>
2023-10-10 20:07:04 -0700

    index.php

--- index.php
+++ index.php
@@ -39,8 +39,7 @@
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-    <noscript data-n-css=""></noscript>
-</head>
+    </head>
 <body>
     <p>Redirect on 5 segund...</p>
 </body>
